9dd002816da6fe24e7d2ff5520863d076db5ae4f
Ryan McIntyre <rmcintyre@acronym.com>
2016-03-29 22:35:51 -0400

    Clean-up

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -21,17 +21,5 @@ class Welcome extends CI_Controller {
 	public function index()
 	{
 		$this->load->view('performers');
-
-		// test db connection and settings:
-		$this->load->database();
-		$query = $this->db->query('SELECT id, act_name FROM performers');
-
-		foreach ($query->result() as $row)
-		{
-			echo $row->id;
-			echo $row->act_name;
-		}
-
-		echo 'Total Results: ' . $query->num_rows();
 	}
 }
